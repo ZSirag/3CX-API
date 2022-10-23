49d72355a7f7749bff1c32ec848b8cf444062122
ZSirag <zahir15mc@outlook.com>
2022-10-23 14:39:00 +0200

    Chiamate Attive

---
+++ library.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import axiosCookieJarSupport from 'axios-cookiejar-support';
+import {CookieJar} from 'tough-cookie';
+
+
+async function login(baseURL, credentials) {
+    const jar = new CookieJar();
+    const http = axios.create({baseURL, withCredentials: true, jar});
+    axiosCookieJarSupport.wrapper(http);
+    if(await loginPOST(http,credentials) !== 'AuthSuccess') {
+        throw new Error('Invalid credentials');
+    }
+    const cookies = jar.getCookiesSync(baseURL);
+    const token = cookies.find((x) => x.key === 'XSRF-TOKEN');
+    if (token) {
+        http.defaults.headers['X-XSRF-TOKEN'] = token.value;
+    }
+    return http;
+}
+
+async function loginPOST(http, credentials){
+    console.log((await http.post("/api/login", credentials)).data);
+    return (await http.post("/api/login", credentials)).data;
+}
+
+
+export {login};
